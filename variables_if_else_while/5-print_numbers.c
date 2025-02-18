20d1b5f08a29aff6dc98072c2254c5f580245e1a
alizium <10549@holbertonstudents.com>
2025-02-18 11:22:32 +0100

    5-print_numbers.c

--- variables_if_else_while/5-print_numbers.c
+++ variables_if_else_while/5-print_numbers.c
@@ -1,15 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
 /**
  * main - Entry point
  *
  * Return: Always 0 (Success)
  */
 
-int numPlaces (int n) {
-    int r = 1;
-    if (n < 0) n = (n == INT_MIN) ? INT_MAX: -n;
-    while (n > 9) {
-        n /= 10;
-        r++;
-    }
-    return r;
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+		printf("%d", i);
+	printf("\n");
+
+	return (0);
 }
