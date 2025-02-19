6f92bd6c53ff8895d49539cd0f8f7853e63b959b
alizium <10549@holbertonstudents.com>
2025-02-19 16:53:20 +0100

    forke

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -8,14 +8,14 @@
  */
 int print_last_digit(int n)
 {
-    int last_digit;
+int last_digit;
 
-    last_digit = n % 10;  /* Extract the last digit */
+last_digit = n % 10;  /* Extract the last digit */
 
-    if (last_digit < 0)  /* Convert to positive if negative */
-        last_digit = -last_digit;
+if (last_digit < 0)  /* Convert to positive if negative */
+last_digit = -last_digit;
 
-    _putchar('0' + last_digit);  /* Print the last digit */
-    
-    return (last_digit);  /* Return the last digit */
+_putchar('0' + last_digit);  /* Print the last digit */
+
+return (last_digit);  /* Return the last digit */
 }
