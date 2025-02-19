23f5a9d5dd208fa011e8a96d293ea99ca3f838c4
alizium <10549@holbertonstudents.com>
2025-02-19 16:22:04 +0100

    new files

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,13 +1,16 @@
 #include "main.h"
 
 /**
- * print_alphabet - Prints the lowercase alphabet followed by a new line
+ * print_alphabet - Prints the alphabet in lowercase followed by a new line
  */
 void print_alphabet(void)
 {
-    char alphabet[] = "abcdefghijklmnopqrstuvwxyz\n";  /* String containing alphabet */
-    int i;
+char alphabet[] = "abcdefghijklmnopqrstuvwxyz";
+int i;
 
-    for (i = 0; alphabet[i] != '\0'; i++)  /* Loop through the string */
-        _putchar(alphabet[i]);  /* Print each character */
+for (i = 0; alphabet[i] != '\0'; i++)
+{
+_putchar(alphabet[i]);
+}
+_putchar('\n');
 }
