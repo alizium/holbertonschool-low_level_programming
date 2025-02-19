2ec2f6f3126fe595f6be6bbaa860d5bdbe137293
alizium <10549@holbertonstudents.com>
2025-02-19 14:55:39 +0100

    new files

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,4 +1,8 @@
-#ifndef MAIN_H
-#define MAIN_H
+#include <unistd.h>
+/**
+ * _putchar - writes characters c to stdout
+ * @c: character to print
+ * Return: On Success 1
+ * On error, -1 is returned, and errno is set appropriately.
+ */
 int _putchar(char c);
-#endif /* MAIN_H */
