33ade7c2cf09bfaf86c389267264f1c5aae1a1b5
alizium <10549@holbertonstudents.com>
2025-02-25 22:10:31 +0100

    kdlo

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -1,21 +1,23 @@
 #include "main.h"
 
 /**
-* print_line - dessine une ligne droite dans le terminal
-* @n: le nombre de fois que le caractère '_' doit être imprimé
+* print_line - draws a straight line in the terminal
+* @n: number of times the character '_' should be printed
 */
 void print_line(int n)
 {
+int i;  /* Déclaration avant la boucle */
+
 if (n <= 0)
 {
-_putchar('\n');  /* Si n <= 0, on imprime juste une nouvelle ligne */
+_putchar('\n');
 }
 else
 {
-for (int i = 0; i < n; i++)
+for (i = 0; i < n; i++)  /* Utilisation de 'i' dans la boucle */
 {
-_putchar('_');  /* Imprimer '_' n fois */
+_putchar('_');
 }
-_putchar('\n');  /* Nouvelle ligne après avoir dessiné la ligne */
+_putchar('\n');
 }
 }
