2654f5c7b3ba80eb7d5e690000c8b47c423e8fe1
alizium <10549@holbertonstudents.com>
2025-02-25 21:38:30 +0100

    dklp

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+* mul - multiplie deux entiers
+* @a: premier entier
+* @b: deuxième entier
+*
+* Return: le produit de a et b
+*/
+int mul(int a, int b)
+{
+return (a * b);
+}
