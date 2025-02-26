a6e3be7c2b9ca336e81015d8c707fee57ff3b9e0
alizium <10549@holbertonstudents.com>
2025-02-26 09:21:21 +0100

    abc

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* _strlen - Retourne la longueur d'une chaîne de caractères
+* @s: Pointeur vers la chaîne de caractères
+*
+* Return: Longueur de la chaîne
+*/
+int _strlen(char *s)
+{
+int len = 0;
+
+while (s[len] != '\0')
+{
+len++;
+}
+return (len);
+}
