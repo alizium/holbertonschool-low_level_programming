07e5dc11e21765f99f6df44369a6aa42f517ecb2
alizium <10549@holbertonstudents.com>
2025-02-28 14:19:34 +0100

    abc

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -6,16 +6,17 @@
 * @s2: Second string.
 *
 * Return: An integer less than, equal to, or greater than 0,
-*         depending on whether s1 is found to be less than,
-*         to match, or be greater than s2.
+* depending on whether s1 is found to be less than,
+* to match, or be greater than s2.
 */
+
 int _strcmp(char *s1, char *s2)
 {
-while (*s1 && (*s1 == *s2))  /* Compare character by character */
+while (*s1 && (*s1 == *s2))
 {
 s1++;
 s2++;
 }
 
-return (*s1 - *s2);  /* Return the difference of the first differing characters */
+return (*s1 - *s2);
 }
