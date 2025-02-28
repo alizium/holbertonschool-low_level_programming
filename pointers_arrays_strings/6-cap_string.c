f1ffca8b4c01100cd41d47e2e460af4c9d4be0a9
alizium <10549@holbertonstudents.com>
2025-02-28 14:39:23 +0100

    abc

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,46 @@
+#include "main.h"
+
+/**
+* cap_string - Capitalizes all words of a string.
+* @s: The string to modify.
+*
+* Return: A pointer to the modified string.
+*/
+char *cap_string(char *s)
+{
+int i = 0;
+int capitalize = 1; /* 1 = prochaine lettre en majuscule */
+
+char separators[] = " \t\n,;.!?\"(){}";
+
+while (s[i] != '\0')
+{
+int j;
+
+/* Vérifier si on est sur un séparateur */
+for (j = 0; separators[j] != '\0'; j++)
+{
+if (s[i] == separators[j])
+{
+capitalize = 1; /* Marquer la prochaine lettre comme majuscule */
+break;
+
+}
+}
+
+/* Si c'est une lettre minuscule et que capitalize est activé */
+if (capitalize && (s[i] >= 'a' && s[i] <= 'z'))
+{
+s[i] -= ('a' - 'A'); /* Convertir en majuscule */
+capitalize = 0; /* Désactiver la capitalisation */
+}
+else if (s[i] >= 'A' && s[i] <= 'Z')
+{
+capitalize = 0; /* Désactiver la capitalisation pour les majuscules */
+}
+
+i++;
+}
+
+return (s);
+}
