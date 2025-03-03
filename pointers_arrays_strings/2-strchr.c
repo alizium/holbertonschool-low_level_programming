7e92ed02e6a833cdec7ad4225b0ee48acbfefe7e
alizium <10549@holbertonstudents.com>
2025-03-03 15:34:47 +0100

    abc

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -1,21 +1,21 @@
 #include "main.h"
 
 /**
- * _strchr - Locates a character in a string.
- * @s: The string to search in.
- * @c: The character to find.
- *
- * Return: Pointer to first occurrence of c in s, or NULL if not found.
- */
+* _strchr - Locates a character in a string.
+* @s: The string to search in.
+* @c: The character to find.
+*
+* Return: Pointer to first occurrence of c in s, or NULL if not found.
+*/
 char *_strchr(char *s, char c)
 {
-    while (*s)
-    {
-        if (*s == c)
-            return (s);
-        s++;
-    }
-    if (c == '\0') /* Vérifie si on cherche la fin de chaîne */
-        return (s);
-    return (NULL);
+while (*s)
+{
+if (*s == c)
+return (s);
+s++;
+}
+if (c == '\0') /* Vérifie si on cherche la fin de chaîne */
+return (s);
+return (NULL);
 }
