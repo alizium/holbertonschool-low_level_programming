496b8ba2aaac7829cace163747f58fffd53554b8
alizium <10549@holbertonstudents.com>
2025-03-03 11:10:32 +0100

    abc

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,5 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <stddef.h>
 
 char *_strcat(char *dest, char *src);
 
@@ -21,5 +22,7 @@ char *_memset(char *s, char b, unsigned int n);
 
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
+
+char *_strchr(char *s, char c);
 
 #endif /* MAIN_H */
