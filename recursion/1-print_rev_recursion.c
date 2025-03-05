4b6cb67504210baf4acf6d282ec4e98f23dccd82
alizium <10549@holbertonstudents.com>
2025-03-05 09:45:57 +0100

    abc

--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -1,16 +1,15 @@
 #include "main.h"
 
 /**
-* _print_rev_recursion - Prints a string in reverse using recursion
-* @s: The string to print in reverse
+* _print_rev_recursion - Prints a string in reverse.
+* @s: The string to be printed.
 */
 void _print_rev_recursion(char *s)
 {
-if (*s == '\0') /* Cas de base : fin de la chaîne */
+if (*s)
 {
-return;
+_print_rev_recursion(s + 1);
+_putchar(*s);
+}
 }
 
-_print_rev_recursion(s + 1); /* Appel récursif pour aller au bout de la chaîne */
-_putchar(*s); /* Affichage en remontant la pile d'appels */
-}
