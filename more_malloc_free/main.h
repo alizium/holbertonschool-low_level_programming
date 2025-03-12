238c1d3833c57022120f68687c76e6a01d9cd892
alizium <10549@holbertonstudents.com>
2025-03-12 15:48:34 +0100

    new

--- more_malloc_free/main.h
+++ more_malloc_free/main.h
@@ -13,4 +13,9 @@ void	*malloc_checked(unsigned int b);
 char	*string_nconcat(char *s1, char *s2, unsigned int n);
 void	*_calloc(unsigned int nmemb, unsigned int size);
 
+void	*malloc_checked(unsigned int b);
+char	*string_nconcat(char *s1, char *s2, unsigned int n);
+void	*_calloc(unsigned int nmemb, unsigned int size);
+int		*array_range(int min, int max);
+
 #endif /* MAIN_H */
