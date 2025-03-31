55faacc53c0339f5bc4cf1e764d1f6b33cd0ac66
alizium <10549@holbertonstudents.com>
2025-03-31 22:33:38 +0200

    fixing () on count

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -2,11 +2,11 @@
 #include "lists.h"
 
 /**
- * print_list - Prints all elements of a list_t list
- * @h: Pointer to the head of the list
- *
- * Return: The number of nodes
- */
+* print_list - Prints all elements of a list_t list
+* @h: Pointer to the head of the list
+*
+* Return: The number of nodes
+*/
 size_t print_list(const list_t *h)
 {
 size_t count = 0;
@@ -27,5 +27,5 @@ count++;
 h = h->next;
 }
 /*Retourne le nombre total de nœuds*/
-return count;
+return (count);
 }
