89c8d4c0ee808dacb42910ec32916e8d2091f7f1
alizium <10549@holbertonstudents.com>
2025-04-03 10:31:21 +0200

    new file

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -1,21 +1,32 @@
 #include "lists.h"
+
 /**
-* add_dnodeint - Adds a node to the beginning of a doubly linked list
-* @head: head of the linked list
-* @n: value to add to the list
-* Return: pointer to the element added
+* add_dnodeint - Adds a new node at the beginning of a dlistint_t list
+* @head: Double pointer to the head of the list
+* @n: Value to store in the new node
+*
+* Return: Address of the new element, or NULL if it failed
 */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-	dlistint_t *tmp;
+	dlistint_t *new_node;
 
-	tmp = *head;
-	tmp = malloc(sizeof(dlistint_t));
-	if (tmp == NULL)
+	/* Allocation du nouveau nœud */
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
 		return (NULL);
-	tmp->n = n;
-	tmp->prev = NULL;
-	tmp->next = *head;
-	*head = tmp;
-	return (tmp);
+
+	/* Initialisation du nouveau nœud */
+	new_node->n = n;
+	new_node->prev = NULL;
+	new_node->next = *head;
+
+	/* Si la liste n'est pas vide, mettre à jour l'ancien head */
+	if (*head != NULL)
+		(*head)->prev = new_node;
+
+	/* Mettre à jour le pointeur head */
+	*head = new_node;
+
+	return (new_node);
 }
