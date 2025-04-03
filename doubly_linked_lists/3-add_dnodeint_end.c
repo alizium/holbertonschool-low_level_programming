1fe0737dcdf0e6665dfcaf251fe2190f5e4d5245
alizium <10549@holbertonstudents.com>
2025-04-03 10:38:22 +0200

    new files

--- doubly_linked_lists/3-add_dnodeint_end.c
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -1,29 +1,27 @@
 #include "lists.h"
 /**
-* *add_dnodeint_end - add a node to the end of the list
-* @head: head of the list
-* @n: value to add to the l
-* Return: pointer to the element added
+* add_dnodeint_end - adds a new node at the end of a dlistint_t list.
+* @head: pointer to list head.
+* @n: integer to be included in the new node.
+* Return: the address of the new element, or NULL if it failed
 */
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
-	dlistint_t *tmp, *new;
+	dlistint_t *tmp_node = NULL, *new_node = malloc(sizeof(dlistint_t));
 
-	new = malloc(sizeof(dlistint_t));
-	if (!new)
-		return (NULL);
-	new->n = n;
-	new->next = NULL;
-	if (!*head)
+	if (new_node)
 	{
-		new->prev = NULL;
-		*head = new;
-		return (*head);
+		new_node->n = n;
+		if (*head)
+		{
+			tmp_node = *head;
+			while (tmp_node->next)
+				tmp_node = tmp_node->next;
+			tmp_node->next = new_node;
+			new_node->prev = tmp_node;
+		}
+		else
+			*head = new_node;
 	}
-	tmp = *head;
-	while (tmp->next)
-		tmp = tmp->next;
-	tmp->next = new;
-	tmp->prev = *head;
-	return (tmp);
+	return (new_node);
 }
