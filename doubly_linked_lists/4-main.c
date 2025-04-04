dfc855df8b789559dbeb01d6b2b3979f8e692bd2
alizium <10549@holbertonstudents.com>
2025-04-04 09:29:34 +0200

    update 4-main.c

--- doubly_linked_lists/4-main.c
+++ doubly_linked_lists/4-main.c
@@ -22,7 +22,7 @@ int main(void)
 	add_dnodeint_end(&head, 402);
 	add_dnodeint_end(&head, 1024);
 	print_dlistint(head);
-	free_dlistint(head);
+	print_dlistint(head);
 	head = NULL;
 	return (EXIT_SUCCESS);
 }
