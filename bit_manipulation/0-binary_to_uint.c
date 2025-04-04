9a122f5ee82c603fc00d4de7647a07eeb8de81c3
alizium <10549@holbertonstudents.com>
2025-04-04 10:28:51 +0200

    modifying for betty

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -4,7 +4,6 @@
 /**
 * binary_to_uint - Function that converts a binary number to an unsigned int.
 * Prototype: unsigned int binary_to_uint(const char *b);
-
 * @b: is pointing to a string of 0 and 1 chars
 * Return: the converted number, or 0 if
 * -> there is one or more chars in the string b that is not 0 or 1
