a5d52dc36c1c2735c9f308151746ac16aa3da379
alizium <10549@holbertonstudents.com>
2025-04-04 14:00:59 +0200

    new files

--- bit_manipulation/2-get_bit.c
+++ bit_manipulation/2-get_bit.c
@@ -1,21 +1,22 @@
 #include <stdio.h>
 #include "main.h"
-
+#include <math.h>
 /**
-* main - Check the code
-*
-* Return: Always 0.
+* get_bit - Write a function that returns the value of a bit at a
+* given index.
+* Prototype: int get_bit(unsigned long int n, unsigned int index);
+
+* @index: is the index, starting from 0 of the bit you want to get
+* @n: bit value in the index.
+* Return: the value of the bit at index index or -1 if an error occured
 */
-int main(void)
+int get_bit(unsigned long int n, unsigned int index)
 {
-	int n;
-
-	n = get_bit(1024, 10);
-	printf("%d\n", n);
-	n = get_bit(98, 1);
-	printf("%d\n", n);
-	n = get_bit(1024, 0);
-	printf("%d\n", n);
-
-	return (0);
+	if (index > 63)
+		return (-1);
+	if (index == 0)
+	{
+		return (n & 1);
+	}
+	return (get_bit(n >> 1, index - 1));
 }
